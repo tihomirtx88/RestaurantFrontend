67af0a60650dd795a233d8d9904e99f2127ea804
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-20 17:10:21 +0300

    implement static home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+
+function HomaPage() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container-xxl py-5 bg-dark hero-header mb-5">
+        <div className="container my-5 py-5">
+          <h1 className="text-white">Enjoy Our Delicious Meal</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomaPage;
