f07841b466d3b26652fbb4158d63da887b64cc27
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-20 17:15:15 +0300

    Implement Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+function Hero() {
+  return (
+    <div className="container-xxl py-5 bg-dark hero-header mb-5">
+      <div className="container my-5 py-5">
+        <div className="row align-items-center g-5">
+          <div className="col-lg-6 text-center text-lg-start">
+            <h1 className="display-3 text-white animated slideInLeft">
+              Enjoy Our <br /> Delicious Meal
+            </h1>
+
+            <p className="text-white animated slideInLeft mb-4 pb-2">
+              Lorem ipsum dolor sit amet...
+            </p>
+
+            <button className="btn btn-primary py-sm-3 px-sm-5 me-3">
+              Book A Table
+            </button>
+          </div>
+
+          <div className="col-lg-6 text-center text-lg-end overflow-hidden">
+            <img className="img-fluid" src="/img/hero.png" alt="hero" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/pages/MenuPage.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+function MenuPage() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h1>Menu</h1>
+      </div>
+    </>
+  );
+}
+
+export default MenuPage;
