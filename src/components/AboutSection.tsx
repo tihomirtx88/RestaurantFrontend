0eb4f13d137d586bdd55c65c42f2382f2c6a06db
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-20 17:18:50 +0300

    Implement AboutSection component

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,23 @@
+function AboutSection() {
+  return (
+    <div className="container-xxl py-5">
+      <div className="container">
+        <div className="row g-5 align-items-center">
+
+          <div className="col-lg-6">
+            <img src="/img/about-1.jpg" className="img-fluid rounded" />
+          </div>
+
+          <div className="col-lg-6">
+            <h5 className="text-primary">About Us</h5>
+            <h1>Welcome to Restoran</h1>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutSection;
