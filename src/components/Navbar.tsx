a287b5766e1c6aec89e3db742d91b64fe61372f8
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-20 16:11:09 +0300

    Adding navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+      <Link to="/" className="navbar-brand p-0">
+        <h1 className="text-primary m-0">
+          <i className="fa fa-utensils me-3"></i>Restoran
+        </h1>
+      </Link>
+
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav ms-auto py-0 pe-4">
+          <Link to="/" className="nav-item nav-link">
+            Home
+          </Link>
+          <Link to="/menu" className="nav-item nav-link">
+            Menu
+          </Link>
+          <Link to="/contact" className="nav-item nav-link">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
