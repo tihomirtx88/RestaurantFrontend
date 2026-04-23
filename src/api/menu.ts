28dc58227756b83cb242bdb03c06eb4964264703
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-23 17:17:09 +0300

    Implement get menu by category

---
+++ src/api/menu.ts
@@ -0,0 +1,10 @@
+import type { MenuItem } from "../types/menu";
+import api from "./axios";
+
+export const get_menu = async(category?: string): Promise<MenuItem[]> =>{
+    const res = await api.get("/menu", {
+        params: category ? {category} : {}
+    })
+
+    return res.data;
+};
