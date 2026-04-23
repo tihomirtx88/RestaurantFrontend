fdf0ba62028a32a0e105401daea5ee6edb5c94fe
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-23 17:27:37 +0300

    implement custom hook useMenu

---
+++ src/hooks/useMenu.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import type { MenuItem } from "../types/menu";
+import { get_menu } from "../api/menu";
+
+export function useMenu(category: string) {
+  const [items, setitems] = useState<MenuItem[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      setLoading(true);
+
+      const data = await get_menu(category);
+      
+      setitems(data);
+      setLoading(false);
+    };
+
+    fetchData();
+
+  }, [category]);
+}
