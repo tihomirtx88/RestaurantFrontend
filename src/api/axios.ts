098deb5565c81596355d6ffad125106425c0139d
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-26 21:10:41 +0300

    Using Axios Interceptor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,4 +6,16 @@ const api = axios.create({
      "Content-Type": "application/json",
   },
 });
+
+// Every request take token for good comunication with backend
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
