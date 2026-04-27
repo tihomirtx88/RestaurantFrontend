0b8631a4848c56e0b0a873e4c89763ad335f2ae0
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-27 14:25:06 +0300

    Implement about page

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,25 @@
+function AboutPage() {
+  return (
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-5 align-items-center">
+
+            <div className="col-lg-6">
+              <img className="img-fluid rounded" src="/img/about-1.jpg" />
+            </div>
+
+            <div className="col-lg-6">
+              <h5 className="text-primary">About Us</h5>
+              <h1>Welcome to Restoran</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default AboutPage;
