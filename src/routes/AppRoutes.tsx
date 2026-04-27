605cada2527a02c27dd51f474a0f6399bfdfd646
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-27 14:48:05 +0300

    Update Approutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,12 +4,23 @@ import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import ProtectedRoute from "./ProtectedRoutes";
 import Layout from "../components/Layout";
+import MenuPage from "../pages/MenuPage";
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+import BookingPage from "../pages/BookingPage";
+import TestimonialPage from "../pages/TestimonialPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/testimonials" element={<TestimonialPage />} />
+
           {/* public  */}
           <Route path="/" element={<HomaPage />} />
 
