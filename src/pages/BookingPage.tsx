39acb194958b31ed757dff506b7a4f57e83e9340
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-29 15:58:13 +0300

    Implement form and dynamics in bookingpage

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,19 +1,69 @@
+import { useState } from "react";
+import { createReservation } from "../api/reservation";
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
+
 function BookingPage() {
+
+    const [date, setDate] = useState("");
+    const [guests, setGuests] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async ()=> {
+      try {
+        setLoading(true);
+
+        await createReservation({
+          reservation_data: date,
+          gueast: Number(guests)
+        });
+
+        toast.success("Reservation created successfully!");
+
+        setDate("");
+        setGuests(1); 
+
+      } catch (err: unknown) {
+        console.log(err);
+        
+        toast.error("Failed to create reservation");
+      }finally{
+        setLoading(false);
+      }
+    };
+
   return (
       <div className="container-xxl py-5">
         <div className="container">
           <h1 className="text-center mb-5">Book A Table</h1>
 
+            {loading && <Loader />}
+
           <div className="row g-5">
             <div className="col-md-6">
-              <input className="form-control mb-3" placeholder="Name" />
-              <input className="form-control mb-3" placeholder="Email" />
-              <input className="form-control mb-3" placeholder="Date & Time" />
-              <input className="form-control mb-3" placeholder="Guests" />
 
-              <button className="btn btn-primary w-100">
-                Book Now
+              <input
+                type="datetime-local"
+                className="form-control mb-3"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+
+              <input
+                type="number"
+                className="form-control mb-3"
+                value={guests}
+                onChange={(e) => setGuests(Number(e.target.value))}
+              />
+
+              <button
+                className="btn btn-primary w-100"
+                onClick={handleSubmit}
+                disabled={loading}
+              >
+                {loading ? "Booking..." : "Book Now"}
               </button>
+
             </div>
 
             <div className="col-md-6">
