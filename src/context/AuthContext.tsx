6877f7136bd4f5ad11a823f79a2110aa44b01e3a
tihomirtx88 <tihomirtx88@gmail.com>
2026-05-07 11:33:42 +0300

    Refactoring AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,32 +2,38 @@ import React, { createContext, useContext, useState } from "react";
 
 type AuthContextType = {
   user: string | null;
-  login: (token: string) => void;
+  role: string | null;
+  login: (token: string, role: string) => void;
   logout: () => void;
 };
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
-export function AuthProvider({children}: {children: React.ReactNode}){
-    const [user, setUser] = useState<string | null>(
-        localStorage.getItem("token")
-    );
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<string | null>(
+    localStorage.getItem("token"),
+  );
+  const [role, setRole] = useState<string | null>(
+    localStorage.getItem("role") 
+  )
 
-    const loginUser = (token: string) => {
-        localStorage.setItem("token", token);
-        setUser(token)
-    }
+  const loginUser = (token: string, role: string) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("role", role);
+    setUser(token);
+    setRole(role);
+  };
 
-    const logout = () => {
-        localStorage.removeItem("token");
-        setUser(null);
-    };
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
 
-    return(
-        <AuthContext.Provider value={{user , login: loginUser, logout}}>
-           {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ user, role, login: loginUser, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
