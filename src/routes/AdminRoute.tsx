78a3658f963601d5136a21cfb541018a32540062
tihomirtx88 <tihomirtx88@gmail.com>
2026-05-07 11:36:39 +0300

    Implement AdminRoute component

---
+++ src/routes/AdminRoute.tsx
@@ -0,0 +1,15 @@
+import type { JSX } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+function AdminRoute({ children }: { children: JSX.Element }) {
+  const { user, role } = useAuth();
+
+  if (!user) return <Navigate to="/login" />;
+
+  if (role !== "admin") return <Navigate to="/" />;
+
+  return children;
+}
+
+export default AdminRoute;
