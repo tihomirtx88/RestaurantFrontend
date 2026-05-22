b713d4ff32119cf4b80e2b9bb0ecf0ca8b78dfe2
tihomirtx88 <tihomirtx88@gmail.com>
2026-05-22 13:31:25 +0300

    Setup tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { AuthProvider } from './context/AuthContext.tsx'
-import ToastProvider from './components/ToastProvider.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import ToastProvider from "./components/ToastProvider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-        <ToastProvider/>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ToastProvider />
         <App />
-    </AuthProvider>
+      </AuthProvider>
+      <ReactQueryDevtools  initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
