320d321b13a43b6ad2849f8877f412e8283636e5
tihomirtx88 <tihomirtx88@gmail.com>
2026-05-24 17:02:26 +0300

    Implement api delete and update review

--- src/api/review.ts
+++ src/api/review.ts
@@ -1,4 +1,4 @@
-import type { Review } from "../types/review";
+import type { CreateReview, Review, UpdateReview } from "../types/review";
 import api from "./axios";
 
 export const getReviews = async (menuItemId: number): Promise<Review[]> => {
@@ -6,11 +6,13 @@ export const getReviews = async (menuItemId: number): Promise<Review[]> => {
   return res.data;
 };
 
-export const createReview = async (data: {
-  rating: number;
-  comment: string;
-  menu_item_id: number;
-}) => {
+export const createReview = async (data: CreateReview) => {
   const res = await api.post("/reviews/", data);
   return res.data;
 };
+
+export const updateReview = (id: number, data: UpdateReview) =>
+  api.patch<Review>(`/reviews/${id}`, data);
+
+export const deleteReview = (id:number) => 
+  api.delete(`/reviews/${id}`)
