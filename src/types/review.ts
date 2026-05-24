6ba066c5a098b1f3fd4550b51b8bfcf48ab9f48a
tihomirtx88 <tihomirtx88@gmail.com>
2026-05-24 16:58:49 +0300

    Implement Review types

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,8 +1,24 @@
 export type Review = {
   id: number;
+
   rating: number;
+
   comment: string;
+
   user_id: number;
+
   menu_item_id: number;
+
   created_at?: string;
 };
+
+export type CreateReview = {
+  rating: number;
+
+  comment: string;
+
+  menu_item_id: number;
+};
+
+export type UpdateReview =
+  Partial<CreateReview>;
